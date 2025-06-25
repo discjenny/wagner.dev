5427290f063a1a37d08b5fe4f2e4a1f5e26889d1
jwagner-mettrix <jwagner@mettrix.com>
2025-06-25 10:25:33 -0400

    bind to ipv4

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ async fn main() {
         .fallback(routes::pages::not_found)
         .layer(axum_mw::from_fn(mw::logger));
 
-    let listener = tokio::net::TcpListener::bind("localhost:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await.unwrap();
     println!("server started");
     axum::serve(listener, app).await.unwrap();
 }
